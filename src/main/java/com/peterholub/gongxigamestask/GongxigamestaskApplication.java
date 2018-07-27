3646aeba7effbae7a85f8df2bd56ebaf6f160b0d
Digital X <winterburn2016>
2018-07-28 01:40:39 +0300

    init

---
+++ src/main/java/com/peterholub/gongxigamestask/GongxigamestaskApplication.java
@@ -0,0 +1,12 @@
+package com.peterholub.gongxigamestask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongxigamestaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongxigamestaskApplication.class, args);
+    }
+}
